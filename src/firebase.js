7272d99cbddc35e9060c5a780e4a2ad011e6b622
kwon <kwon844@email.daelim.ac.kr>
2022-03-30 19:53:43 +0900

    firebase.js commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDujK9dnRSyoOAjS0b5fzxPePrUwzu6RW4",
+    authDomain: "nwitter-8fcaf.firebaseapp.com",
+    projectId: "nwitter-8fcaf",
+    storageBucket: "nwitter-8fcaf.appspot.com",
+    messagingSenderId: "1060641010090",
+    appId: "1:1060641010090:web:52f55515ecfc1b86683bd3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
